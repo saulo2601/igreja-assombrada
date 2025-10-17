b0cde3d28454765c35cc993c152d3219f89470cd
saulo2601 <saulo.barboza@escola.pr.gov.br>
2025-10-17 09:05:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avança=document.querySelectorAll|{'.btn-proximo'}
+
+avança.forEach(button)
+button.addEventlistener('clik',function(){
+const atual=document.querySelectorAll('.ativo')
+const
+
+atual
+
+})
